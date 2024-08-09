413d7ad7d75374eb43466b476605e06a1ec063ba
zntb <zntbdev@proton.me>
2024-08-09 15:22:32 +0300

    Refactor magic number

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -7,6 +7,9 @@ import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 contract HelperConfig is Script {
     NetworkConfig public activeNetworkconfig;
 
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
     struct NetworkConfig {
         address priceFeed; // ETHUSD price feed address
     }
@@ -30,7 +33,10 @@ contract HelperConfig is Script {
     function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
         vm.startBroadcast();
 
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
 
         vm.stopBroadcast();
 
