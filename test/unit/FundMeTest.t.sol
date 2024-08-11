321f3eb3669a76cbe6c0492bff31612fdef23bfe
zntb <zenetibi@yahoo.com>
2024-08-11 12:11:14 +0300

    Remove broadcast/ and lib/ from tracking

--- test/unit/FundMeTest.t.sol
+++ test/unit/FundMeTest.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../../src/FundMe.sol";
